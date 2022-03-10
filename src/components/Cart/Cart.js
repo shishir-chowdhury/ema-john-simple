58974da2ee33590178a713f8cb802d84ae59415d
Shishir <shiishir@gmail.com>
2022-03-10 23:20:32 +0600

    adding grand total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,11 +6,22 @@ const cart = (props) => {
     for (const product of cart) {
         total = total + product.price
     }
+
+    const shipping = 15;
+    const tax = total * 0.1;
+    const grandTotal = total + shipping + tax;
+    console.log(grandTotal);
+
+
     return (
         <div>
             <h2>Order Summary</h2>
             <h3>Item Ordered: {props.cart.length}</h3>
             <h3>Total price: {total}</h3>
+            <p>Shipping: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <h2>Grand Total: {grandTotal}</h2>
+
 
         </div>
     );
