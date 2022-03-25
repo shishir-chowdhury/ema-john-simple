9dc8463c5f4060b8fdd35e7f97eece83444fa3bc
Shishir <shiishir@gmail.com>
2022-03-25 22:05:15 +0600

    React ratting with font awesome

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+import Rating from 'react-rating';
 
 const Product = (props) => {
     // console.log(props)
-    const { name, img, seller, price, stock } = props.product;
+    const { name, img, seller, price, stock, star } = props.product;
 
     const element = <FontAwesomeIcon icon={faShoppingCart} />;
 
@@ -19,6 +20,13 @@ const Product = (props) => {
                 <h3>by: {seller}</h3>
                 <h3>Price: {price}</h3>
                 <p><small>only {stock} left in stoct - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly>
+                </Rating>
+                <br />
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
                     className='btn-regular'>{element}Add to cart
